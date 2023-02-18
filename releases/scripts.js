b3073cd2a02bdb0f5b21598ec189a4b4622c65a2
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-02-19 08:56:31 +1100

    update

--- releases/scripts.js
+++ releases/scripts.js
@@ -1,33 +1,5 @@
 function downloadClient(version) {
     if (version == 0.1) {
-        getFileContent("content/0.1/installer.exe").then((content) => {
-            save("installer.exe", content);
-        });
+        window.location.href = "/content/0.1/installer.exe";
     }
-}
-
-function save(filename, data) {
-    const blob = new Blob([data], {type: 'text/csv'});
-    if(window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, filename);
-    }
-    else{
-        const elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename;        
-        document.body.appendChild(elem);
-        elem.click();        
-        document.body.removeChild(elem);
-    }
-}
-
-function getFileContent(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-            resolve(event.target.result);
-        };
-        reader.onerror = reject;
-        reader.readAsText(file);
-    });
 }
